0b65c183c5607baeb5a117592e8f524fd65d1544
BilalMahfouf <billelmh501@gmail.com>
2026-05-25 10:59:39 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-// #include <string>
+#include <string>
+
+std::string readUserCommand() {
+
+  std::string message = "";
+  std::cout << "$ ";
+  std::cin >> message;
+  return message;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  const std::string command = readUserCommand();
+
+  std::cout << command << ": command not found";
 }
