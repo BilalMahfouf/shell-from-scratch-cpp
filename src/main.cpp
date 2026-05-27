a717f0e1ca7b771c226f98022de59b4bb6b2c568
BilalMahfouf <billelmh501@gmail.com>
2026-05-27 21:32:38 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -152,7 +152,8 @@ void cd(const std::string &absolutePath) {
       fs::current_path(absolutePath);
       return;
     } catch (const fs::filesystem_error &e) {
-      std::cout << "cd: " << absolutePath << " No such file or directory";
+      std::cout << "cd: " << absolutePath << " No such file or directory"
+                << endl;
       // std::cerr << "Error: " << e.what() << std::endl;
     }
   } else {
